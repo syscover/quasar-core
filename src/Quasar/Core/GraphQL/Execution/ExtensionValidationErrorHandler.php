6e72365dbc70e1fdd0d17e9ec803061854b6af03
Carlos Rodríguez Palacín <carlos.rodriguez@techedgegroup.es>
2019-12-13 01:57:29 +0100

    add validation to graphQL

---
+++ src/Quasar/Core/GraphQL/Execution/ExtensionValidationErrorHandler.php
@@ -0,0 +1,37 @@
+<?php namespace Quasar\Core\GraphQL\Execution;
+
+use GraphQL\Error\Error;
+use Nuwave\Lighthouse\Execution\ErrorHandler;
+use Illuminate\Validation\ValidationException;
+
+/**
+ * Handle Exceptions that implement Nuwave\Lighthouse\Exceptions\RendersErrorsExtensions
+ * and add extra content from them to the 'extensions' key of the Error that is rendered
+ * to the User.
+ */
+class ExtensionValidationErrorHandler implements ErrorHandler
+{
+    public static function handle(Error $error, \Closure $next): array
+    {
+        // If an error was caused by exception thrown in resolver, $error->getPrevious() would contain original exception
+        // in this case, we try catch ValidationException
+        $underlyingException = $error->getPrevious();
+
+        if ($underlyingException && $underlyingException instanceof ValidationException) {
+            // Reconstruct the error, passing in the extensions of the underlying exception
+            $error = new Error(
+                $error->message,
+                $error->nodes,
+                $error->getSource(),
+                $error->getPositions(),
+                $error->getPath(),
+                $underlyingException,
+                [
+                    'validationErrors' => $underlyingException->errors()
+                ]
+            );
+        }
+
+        return $next($error);
+    }
+}
