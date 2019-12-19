ca2549cc2df931b85009db887824905ab0ba6d3e
Carlos Rodríguez Palacín <carlos.rodriguez@techedgegroup.es>
2019-12-19 02:30:02 +0100

    add config resolvers

---
+++ src/Quasar/Core/GraphQL/Resolvers/ConfigResolver.php
@@ -0,0 +1,26 @@
+<?php namespace Quasar\Core\GraphQL\Resolvers;
+
+use Illuminate\Support\Facades\App;
+
+class ConfigResolver
+{
+    public function index($root, array $args)
+    {
+        $config = config($args['config']['key']);
+
+        if(isset($args['config']['lang']) && isset($args['config']['property']))
+        {
+            // set lang
+            App::setLocale($args['config']['lang']);
+            $property = $args['config']['property'];
+
+            // translate property indicated
+            $config = array_map(function($object) use ($property) {
+                $object->{$property} = trans($object->{$property});
+                return $object;
+            }, $config);
+        }
+
+        return $config;
+    }
+}
