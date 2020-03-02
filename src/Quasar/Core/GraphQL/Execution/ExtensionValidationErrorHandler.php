5f9220de2a4a357c5543a992314065f648cd6202
Carlos <carlos.rodriguez@techedgegroup.com>
2020-03-02 18:21:29 +0100

    add custom fields for GraphQL exceptions

--- src/Quasar/Core/GraphQL/Execution/ExtensionValidationErrorHandler.php
+++ src/Quasar/Core/GraphQL/Execution/ExtensionValidationErrorHandler.php
@@ -3,6 +3,7 @@
 use GraphQL\Error\Error;
 use Nuwave\Lighthouse\Execution\ErrorHandler;
 use Illuminate\Validation\ValidationException;
+use Quasar\OAuth\Exceptions\AuthenticationException;
 
 /**
  * Handle Exceptions that implement Nuwave\Lighthouse\Exceptions\RendersErrorsExtensions
@@ -17,8 +18,15 @@ class ExtensionValidationErrorHandler implements ErrorHandler
         // in this case, we try catch ValidationException
         $underlyingException = $error->getPrevious();
 
-        if ($underlyingException && $underlyingException instanceof ValidationException) 
+        if (
+            $underlyingException && $underlyingException instanceof ValidationException ||
+            $underlyingException && $underlyingException instanceof AuthenticationException
+        ) 
         {
+            // add custom fields for exception
+            $extensions = ['code' => $underlyingException->getCode()];
+            if ($underlyingException instanceof ValidationException) $extensions['validationErrors'] = $underlyingException->errors();
+
             // Reconstruct the error, passing in the extensions of the underlying exception
             $error = new Error(
                 $error->message,
@@ -27,9 +35,7 @@ class ExtensionValidationErrorHandler implements ErrorHandler
                 $error->getPositions(),
                 $error->getPath(),
                 $underlyingException,
-                [
-                    'validationErrors' => $underlyingException->errors()
-                ]
+                $extensions
             );
         }
 
